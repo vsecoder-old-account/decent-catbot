87a2474ef7c7829c80633af024d89cda00e7c375
kreepmeister <htmlcssphpjs@yandex.ru>
2022-06-13 17:56:56 +0300

    Add db and edit README.md

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Decent cat bot
 
+[![wakatime](https://wakatime.com/badge/user/17993a3c-e23b-43ce-a9c6-84b6248d1411/project/59440ada-465f-445c-913f-ef194a24ba40.svg)](https://wakatime.com/badge/user/17993a3c-e23b-43ce-a9c6-84b6248d1411/project/59440ada-465f-445c-913f-ef194a24ba40)
+
 A bot for multifunctional chat moderation.
+
+The bot has both the usual commands (ban, unban, kick, mute, unmute) and special protections that can be configured and used, their list is:
+ * "<b> 🐵 <code>AntiTagAll</code> - Restricts the tag of all participants</b>"
+ * "<b> 👋 <code>Welcome</code> - Welcomes new members</b>"
+ * "<b> 🐶 <code>AntiRaid</code> - Bans new members</b>(in dev)"
+ * "<b> 📯 <code>AntiChannel</code> - Prohibits writing messages on behalf of channels</b>"
+ * "<b> 🎑 <code>AntiGIF</code> - Prohibits GIFs</b>"
+ * "<b> 🍓 <code>AntiNSFW</code> - Prohibits NSFW photos and stickers</b>(now off)"
+ * "<b> ⏱ <code>AntiFlood</code> - Prohibits flooding</b>(in dev)"
+ * "<b> 😒 <code>AntiExplicit</code> - Prohibits swearing </b>"
+ * "<b>🥷 <code>BanNinja</code> - Automatic version of the protection of AntiRaid(</b>in dev)"
+ * "<b> 👾 Admin: </b><code>/ban</code> <code>/kick</code> <code>/mute</code>"
+ * "<code>/unban</code><code>/unmute</code> <b>- Admin tools</b>"
+ * "<b> 👮‍♂️ Warnings:</b> <code>/warn</code> <code>/warns</code>(in dev)"
+ * "<b> 🗒 Notes:</b> <code>/nsave</code> <code>/nstop</code> <code>/notes</code> - notes"
+ * "<b> ⏳ Configure protection:</b> <code>/setprotect {name} [on/off]</code>"
---
+++ db/funcs.py
@@ -0,0 +1,95 @@
+from tinydb import TinyDB, Query
+
+db = TinyDB('../db.json')
+Chat = Query()
+
+"""
+[
+    {
+        "id": '1234567890',
+        "antichannel": False,
+        "antigif": False,
+        "antiraid": False,
+        "antiexplicit": False,
+        "antitagall": False,
+        "antitarab": False,
+        "antinsfw": False,
+        "antiflood": False,
+        "banninja": False,
+        "welcome": True,
+        "welcome_text": "✌️ Hi, {mention}!",
+        "report": True,
+        "warns": [{"id": "1234567890", "reasons": ["spam", "flood"]}],
+        "notes": [{"name": "dev", "text": "@vsecoder"}]
+    }
+]
+"""
+
+def init_chat(id):
+    db.insert({
+        "id": id,
+        "antichannel": False,
+        "antigif": False,
+        "antiraid": False,
+        "antiexplicit": False,
+        "antitagall": False,
+        "antitarab": False,
+        "antinsfw": False,
+        "antiflood": False,
+        "banninja": False,
+        "welcome": True,
+        "welcome_text": "✌️ Hi, {mention}!",
+        "report": True,
+        "warns": [],
+        "notes": [{"name": "dev", "text": "@vsecoder"}]
+    })
+
+def check_protected(id, protect):
+    try:
+        return db.search(Chat.id == id)[0][protect]
+    except:
+        return False
+
+def create_note(id, name, text):
+    notes = db.search(Chat.id == id)[0]["notes"]
+    notes.append({"name": name, "text": text})
+    db.update({"notes": notes}, Chat.id == id)
+    return True
+
+def delete_note(id, name):
+    notes = db.search(Chat.id == id)[0]["notes"]
+    for note in notes:
+        if note["name"] == name:
+            notes.remove(note)
+            db.update({"notes": notes}, Chat.id == id)
+            return True
+    return False
+
+def get_notes(id):
+    notes = db.search(Chat.id == id)[0]["notes"]
+    return notes
+
+def get_note(id, name):
+    notes = db.search(Chat.id == id)[0]["notes"]
+    for note in notes:
+        if note["name"] == name:
+            return note["text"]
+
+def create_warn(id, text):
+    warns = db.search(Chat.id == id)[0]["warns"]
+    warns.append(text)
+    db.update({"warns": warns}, Chat.id == id)
+    return True
+
+def delete_warn(id, text):
+    warns = db.search(Chat.id == id)[0]["warns"]
+    for warn in warns:
+        if warn == text:
+            warns.remove(warn)
+            db.update({"warns": warns}, Chat.id == id)
+            return True
+    return False
+
+def get_warns(id, user_id):
+    warns = db.search(Chat.id == id)[0]["warns"][user_id]
+    return warns
